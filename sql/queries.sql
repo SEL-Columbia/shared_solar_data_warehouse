1b35e7670829e604e0253e077b0b4e03ae7c6fff
chrisnatali <chris.natali@gmail.com>
2013-10-23 21:00:21 +0000

    refined table defs

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,3 +1,2 @@
-
 -- Queries for data analysis go here
-
+select count(*) as ct, site_id, machine_id, ip_addr, time_stamp from raw_circuit_reading group by site_id, machine_id, ip_addr, time_stamp having count(*) > 2;
--- sql/tables.sql
+++ sql/tables.sql
@@ -2,9 +2,9 @@
 
 CREATE TABLE circuit (
   pk           uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
-  machine_id   varchar(18) NOT NULL,
-  site_id      varchar(12) NOT NULL,
-  ip_addr      varchar(46) NOT NULL, -- size is ipv6 ready
+  machine_id   bigint NOT NULL,
+  site_id      varchar(8) NOT NULL,
+  ip_addr      varchar(16) NOT NULL, -- size is ipv6 ready
   main_circuit boolean DEFAULT false,
   UNIQUE(machine_id, site_id, ip_addr)
 );
@@ -13,22 +13,21 @@ CREATE TABLE power_reading (
   pk               uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
   circuit          uuid REFERENCES circuit (pk),
   time_stamp       timestamp,
-  watts            numeric,
-  volts            numeric,
-  amps             numeric,
-  watt_hours_sc20  numeric,
-  watt_hours_today numeric,
-  max_watts        numeric,
-  max_volts        numeric,
-  max_amps         numeric,
-  min_watts        numeric,
-  min_volts        numeric,
-  min_amps         numeric,
-  power_factor     numeric,
-  power_cycle      numeric,
-  frequency        numeric,
-  volt_amps        numeric,
-  relay_not_closed boolean,
-  send_rate        numeric,
-  credit           numeric
+  watts            real,
+  watt_hours_sc20  double precision,
+  credit           real 
+);
+
+-- table definitions for the *raw* Shared Solar SD Logs
+CREATE TABLE raw_circuit_reading (
+  drop_id          varchar(8) NOT NULL,  --YYYYMMDD format
+  line_num         smallint NOT NULL,
+  site_id          varchar(8) NOT NULL,
+  machine_id       bigint NOT NULL,
+  ip_addr          varchar(16) NOT NULL,
+  circuit_type     varchar(10) NOT NULL,
+  time_stamp       varchar(14) NOT NULL, --YYYYMMDDHHmmss format
+  watts            real,
+  watt_hours_sc20  double precision,
+  credit           real
 );
