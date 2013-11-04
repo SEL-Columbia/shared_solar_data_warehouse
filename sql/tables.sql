529c35d12871ca2e3e5aae048a41dc8bba641ab3
chrisnatali <chris.natali@gmail.com>
2013-11-04 14:57:14 -0500

    commented out normalized tables for now

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,24 +1,24 @@
 -- table definitions for the Shared Solar SD Log database
 
-CREATE TABLE circuit (
-  pk bigserial primary key,
-  --pk           SERIAL PRIMARY KEY,
-  machine_id   bigint NOT NULL,
-  site_id      varchar(8) NOT NULL,
-  ip_addr      varchar(16) NOT NULL, -- size is ipv6 ready
-  main_circuit boolean DEFAULT false,
-  UNIQUE(machine_id, site_id, ip_addr)
-);
-
-CREATE TABLE power_reading (
-  pk bigserial primary key,
-  --pk               SERIAL PRIMARY KEY,
-  circuit          bigint REFERENCES circuit (pk),
-  time_stamp       timestamp without time zone,
-  watts            real,
-  watt_hours_sc20  double precision,
-  credit           real 
-);
+-- CREATE TABLE circuit (
+--   pk bigserial primary key,
+--   --pk           SERIAL PRIMARY KEY,
+--   machine_id   bigint NOT NULL,
+--   site_id      varchar(8) NOT NULL,
+--   ip_addr      varchar(16) NOT NULL, -- size is ipv6 ready
+--   main_circuit boolean DEFAULT false,
+--   UNIQUE(machine_id, site_id, ip_addr)
+-- );
+-- 
+-- CREATE TABLE power_reading (
+--   pk bigserial primary key,
+--   --pk               SERIAL PRIMARY KEY,
+--   circuit          bigint REFERENCES circuit (pk),
+--   time_stamp       timestamp without time zone,
+--   watts            real,
+--   watt_hours_sc20  double precision,
+--   credit           real 
+-- );
 
 -- table for the *raw* Shared Solar SD Logs
 CREATE TABLE IF NOT EXISTS raw_circuit_reading (
