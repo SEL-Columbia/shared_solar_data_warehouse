cfafa667023d7a1c593e310965defd5f6c31eb36
Denis Papathanasiou <dp2704@columbia.edu>
2013-11-06 15:43:06 -0500

    Since psycopg2's executemany() statement rejects an entire block of insert dicts when only one of them fails a uniqueness constraint, and from a performance perspective doing multiple execute() statements is just as fast, restructed db_utils.insert() accordingly; also unified the error reporting using psycopg2.Error and the code and message it returns.

--- processor/db_utils.py
+++ processor/db_utils.py
@@ -18,8 +18,8 @@ def connect (dbname, dbuser, pwd=None):
         if pwd is not None:
             conn += " password='"+pwd+"'"
         return psycopg2.connect(conn)
-    except psycopg2.DatabaseError, db_err:
-        print >> sys.stderr, 'Error:', db_err
+    except psycopg2.Error, db_err:
+        print >> sys.stderr, db_err.pgcode, db_err.pgerror
 
 def search (conn, select_string, select_params={}, close_conn=False):
     """Use the database connection to execute a select query and return
@@ -29,8 +29,12 @@ def search (conn, select_string, select_params={}, close_conn=False):
     cur = conn.cursor()
 
     # Query the database and obtain data as Python objects
-    cur.execute(select_string, select_params)
-    results = cur.fetchall()
+    try:
+        cur.execute(select_string, select_params)
+        results = cur.fetchall()
+    except psycopg2.Error, db_err:
+        print >> sys.stderr, db_err.pgcode, db_err.pgerror
+        results = None
 
     # Close communication with the database
     cur.close()
@@ -48,29 +52,23 @@ def insert (conn, table, columns, inserts, close_conn=False):
     # Open a cursor to perform database operations
     cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
+    # create the statement to which insert data will be bound
+    # (psycopg does not support the notion of prepared statements,
+    #  but we can simulate that here, by establishing the core
+    #  insert statement once, for which all key/value pairs map)
+    column_bindings = map(lambda x: '%('+x+')s', columns)
+    insert_stmt = ' '.join(["INSERT INTO", table,
+                            "(", ','.join(columns), ")",
+                            "VALUES (", ','.join(column_bindings), ")"])
+                                 
     # process each {columns=values} dict
-    try:
-        column_bindings = map(lambda x: '%('+x+')s', columns)
-        cur.executemany("INSERT INTO "+ table +
-                        " ("+ ','.join(columns) +") VALUES ("+
-                        ','.join(column_bindings)+")",
-                        inserts)
-    except KeyError:
-        # this happened b/c one or more dicts in the inserts list
-        # did not match the keys as defined in the columns list
-        # so display the error and rollback the transaction
-        conn.rollback()
-        print >> sys.stderr, 'Error: bad inserts', inserts 
-    except psycopg2.IntegrityError:
-        # if we wind up trying to insert a duplicate (violating a
-        # table UNIQUE constraint) let it go by committing
-        # everything to this point, and continue
-        conn.commit()
-    except psycopg2.OperationalError, op_err:
-        print >> sys.stderr, 'Error:', op_err
-
-    # Make the changes to the database persistent
-    conn.commit()
+    for insert_dict in inserts:
+        try:
+            cur.execute(insert_stmt, insert_dict)
+            conn.commit()
+        except psycopg2.Error, ins_err:
+            print >> sys.stderr, ins_err.pgcode, ins_err.pgerror
+            conn.rollback()
 
     # Close communication with the database
     cur.close()
