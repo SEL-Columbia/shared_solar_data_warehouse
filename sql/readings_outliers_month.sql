56652fd66fce79b223775cd6817d0b0b9da02c15
chrisnatali <chris.natali@gmail.com>
2013-12-27 16:38:06 -0500

    renamed table in sql.  added filters for more meaningful outlier graph

--- sql/readings_outliers_month.sql
+++ sql/readings_outliers_month.sql
@@ -1,2 +1,2 @@
-select count(*) ct, site_id, ip_addr, date_trunc('month', time_stamp) time_stamp into tmp_circuit_outliers_by_month from circuit_reading_outliers group by site_id, ip_addr, date_trunc('month', time_stamp);
+select count(*) ct, site_id, ip_addr, date_trunc('month', time_stamp) time_stamp into tmp_circuit_outliers_by_month from tmp_circuit_reading_outliers group by site_id, ip_addr, date_trunc('month', time_stamp);
 select count(*) ct, site_id, ip_addr, date_trunc('month', time_stamp) time_stamp into tmp_circuit_readings_by_month from circuit_reading group by site_id, ip_addr, date_trunc('month', time_stamp);
